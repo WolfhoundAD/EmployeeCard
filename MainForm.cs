431406d3ae8b50515710b9cd9c73e2513bbf4838
WolfhoundAD <Loners14@yandex.ru>
2023-10-12 12:51:34 +0300

    first commit

---
+++ MainForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EmployeeCard
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "employeeBDDataSet.EmplWorkData". При необходимости она может быть перемещена или удалена.
+            this.emplWorkDataTableAdapter.Fill(this.employeeBDDataSet.EmplWorkData);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "employeeBDDataSet.EmployeePersonalData". При необходимости она может быть перемещена или удалена.
+            this.employeePersonalDataTableAdapter.Fill(this.employeeBDDataSet.EmployeePersonalData);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "employeeBDDataSet.Employees". При необходимости она может быть перемещена или удалена.
+            this.employeesTableAdapter.Fill(this.employeeBDDataSet.Employees);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "employeeBDDataSet.Departments". При необходимости она может быть перемещена или удалена.
+            this.departmentsTableAdapter.Fill(this.employeeBDDataSet.Departments);
+            addresTxt.ReadOnly = true;
+            educationTxt.ReadOnly = true;
+          
+
+
+        }
+
+        private void tableLayoutPanel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Utils/DBHelper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace EmployeeCard.Utils
+{
+   public enum TableFieldTypes
+    {
+        integer,
+        nvarchar
+    }
+
+    public class TableField
+    {
+        public string TableFieldValue { get; set; }
+        public TableFieldTypes TableFieldType { get; set; }
+    }
+    public static class DBHelper
+    {
+        public static bool InsertEntry(string tableName, Dictionary<string, TableField> fields)
+        {
+            try
+            {
+                var conn = new SqlConnection(Properties.Settings.Default.EmployeeDBCConnectionString);
+                var fieldsNames = string.Join(",", fields.Select(f => f.Key));
+                var fieldsValues = string.Join(",", fields.Select(f =>
+                {
+                    if (f.Value.TableFieldType == TableFieldTypes.integer)
+                    {
+                        return f.Value.TableFieldValue;
+                    }
+                    return $"'{f.Value.TableFieldValue}'";
+                }));
+                var query = $"INSERT INTO {tableName} ({fieldsNames}) VALUES ({fieldsValues})";
+                var cmd = new SqlCommand(query, conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public static bool UpdateEntry(string tableName, int id, Dictionary<string, TableField> fields)
+        {
+            try
+            { 
+            var conn = new SqlConnection(Properties.Settings.Default.EmployeeDBCConnectionString);
+            var updatingFieldsValues = string.Join(",", fields.Select(f =>
+            {
+                var fieldValue = string.Empty;
+                if (f.Value.TableFieldType == TableFieldTypes.integer)
+                {
+                    fieldValue = f.Value.TableFieldValue;
+                }
+                else
+                {
+                    fieldValue = $"'{f.Value.TableFieldValue}'";
+                }
+                return $"{f.Key}= {fieldValue}";
+            }));
+            var query = $"UPDATE {tableName} SET {updatingFieldsValues} WHERE Id = {id}";
+            var cmd = new SqlCommand(query, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            return true;
+        }
+              catch
+                {
+              return false;
+                    }
+    }
+        public static bool DeleteEntry(string tableName, int id)
+        {
+            try
+            {
+                //возможно EmployeesBDConnectionString
+                var conn = new SqlConnection(Properties.Settings.Default.EmployeeDBCConnectionString);
+                var query = $"DELETE FROM {tableName} WHERE Id={id}";
+                var cmd = new SqlCommand(query, conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    } 
+}
